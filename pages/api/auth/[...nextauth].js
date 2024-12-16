306de705d9dabde4928f1510aa109937c9d07971
Zain-appiness <zain@appinessworld.com>
2024-12-16 11:16:52 +0530

    DEPLOYEMENT ON VERCEL

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,6 +6,7 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      redirectUri: "https://next-auth-frontend-olv3-bmjbzlt0g-zain-appiness-projects.vercel.app/api/auth/callback/google",
       authorization: {
         params: {
           prompt: "consent",
